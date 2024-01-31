c55c43e0d62b48e4e17abebd189bc7e49cde5f1b
Vo Van Tra <travv@runsystem.net>
2024-01-31 17:03:13 +0700

    TOP page use

--- src/services/axios/offices/query.js
+++ src/services/axios/offices/query.js
@@ -4,3 +4,8 @@ export async function getOfficesQuery(){
   const response = await getHttp(`api/v1/offices`);
   return response.data;
 }
+
+export async function getTopInOfficesQuery(office_id){
+  const response = await getHttp(`api/v1/offices/${office_id}/top`);
+  return response.data;
+}
